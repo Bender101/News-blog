5d2f534ecdb19de4b00c98620166c7af463da2d7
bender101 <sergkok227@gmail.com>
2023-05-30 17:39:13 +0300

    fix gitignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,33 @@
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: /\.module\.\w+$/i,
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  //If we don't use typescript, we should use babel
+  const typescriptLoaders = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoaders, cssLoaders];
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,33 @@
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import path from "path";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
+
+export function BuildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
+
+  return {
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+
+    plugins: buildPlugins(options),
+
+    mode: mode,
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+}
