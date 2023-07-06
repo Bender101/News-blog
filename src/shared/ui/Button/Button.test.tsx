38db28de7c1aa302d5ab8ae703ab567e5adfb2ff
bender101 <sergkok227@gmail.com>
2023-07-06 18:52:23 +0300

    remove screen debug

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,15 +1,14 @@
-import {Button, ThemeButton} from "shared/ui/Button/Button";
-import {render, screen} from "@testing-library/react";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+import { render, screen } from "@testing-library/react";
 
 describe("Button", () => {
   test("render button", () => {
     render(<Button>TEST</Button>);
-    expect(screen.getByText('TEST')).toBeInTheDocument();
+    expect(screen.getByText("TEST")).toBeInTheDocument();
   });
 
   test("add class button", () => {
     render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
-    expect(screen.getByText('TEST')).toHaveClass('clear');
-    screen.debug()
+    expect(screen.getByText("TEST")).toHaveClass("clear");
   });
 });
