d899aae6e4c558a5d43d797678b8b5c1b72ef190
bender101 <sergkok227@gmail.com>
2023-07-08 12:04:32 +0300

    fix lint err

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
 
 interface UseThemeResult {
@@ -13,5 +13,5 @@ export const useTheme = (): UseThemeResult => {
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
-  return { theme, toggleTheme }
+  return { theme, toggleTheme };
 };
