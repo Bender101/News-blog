2e81c5c0073199236aa18386548efe571d7d44d9
bender101 <sergkok227@gmail.com>
2023-08-30 12:47:27 +0300

    add Store Decorator to MainPage.stories

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import MainPage from "./MainPage";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 const meta: Meta<typeof MainPage> = {
   title: "pages/MainPage",
@@ -13,9 +14,11 @@ export default meta;
 type Story = StoryObj<typeof MainPage>;
 
 export const MainPageLight: Story = {
+  decorators: [StoreDecorator({})],
+
   args: {},
 };
 export const MainPageDark: Story = {
-  decorators: [ThemeDecorator(Theme.DARK)],
+  decorators: [ThemeDecorator(Theme.DARK), StoreDecorator({})],
   args: {},
 };
