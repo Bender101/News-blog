29c29913700dc20ee8a53fe426f2390b5a16b3e6
bender101 <sergkok227@gmail.com>
2023-10-09 15:51:45 +0300

    setup and ready to deploy

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,7 @@ import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
-
+import CopyPlugin  from "copy-webpack-plugin";
 
 export function buildPlugins({
   paths,
@@ -25,6 +25,11 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project)
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: paths.locales, to: paths.buildLocales },
+      ],
     }),
   ];
 
