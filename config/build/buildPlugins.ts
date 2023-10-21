7deaf5a3537b680b2f05a6b66293d382a1c2b42e
bender101 <sergkok227@gmail.com>
2023-10-21 17:32:43 +0300

    add CircularDependencyPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,8 @@ import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
-import CopyPlugin  from "copy-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import CircularDependencyPlugin from "circular-dependency-plugin";
 
 export function buildPlugins({
   paths,
@@ -24,18 +25,20 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
-      __PROJECT__: JSON.stringify(project)
+      __PROJECT__: JSON.stringify(project),
     }),
     new CopyPlugin({
-      patterns: [
-        { from: paths.locales, to: paths.buildLocales },
-      ],
+      patterns: [{ from: paths.locales, to: paths.buildLocales }],
     }),
   ];
 
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin());
     plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    plugins.push(new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true
+    }))
   }
 
   return plugins;
