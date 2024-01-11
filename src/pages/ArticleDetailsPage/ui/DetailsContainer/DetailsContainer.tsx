ca2e2380bc5b3a138f6f01a4c0861b92719e373d
bender101 <sergkok227@gmail.com>
2024-01-11 10:04:03 +0300

    add edit widget, details container, plural forms

---
+++ src/pages/ArticleDetailsPage/ui/DetailsContainer/DetailsContainer.tsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+import { useParams } from 'react-router-dom';
+import { ArticleDetails } from '@/entities/Article';
+import { Card } from '@/shared/ui/redesigned/Card';
+
+interface DetailsContainerProps {
+    className?: string;
+}
+
+export const DetailsContainer = memo((props: DetailsContainerProps) => {
+    const { className } = props;
+    const { id } = useParams<{ id: string }>();
+
+    return (
+        <Card max border="round" className={className} padding="24">
+            <ArticleDetails id={id} />
+        </Card>
+    );
+});
