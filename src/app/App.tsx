2baf70518d457b711119d84e044501f0bcf3798c
bender101 <sergkok227@gmail.com>
2024-01-15 09:42:50 +0300

    scroll toolbar widget, useAppToolbar, useRouteChange

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,23 @@
-import React, { Suspense, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import { getUserInited, initAuthData } from "@/entities/User";
-import { AppRouter } from "./providers/router";
-import { Navbar } from "@/widgets/Navbar";
-import { SideBar } from "@/widgets/SideBar";
-import { useTheme } from "@/shared/lib/hooks/useTheme/useTheme";
-import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
-import { ToggleFeatures } from "@/shared/lib/features";
-import { MainLayout } from "@/shared/layouts/MainLayout";
-import { AppLoaderLayout } from "@/shared/layouts/AppLoaderLayout";
-import { PageLoader } from "@/widgets/PageLoader";
+import React, { Suspense, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { getUserInited, initAuthData } from '@/entities/User';
+import { AppRouter } from './providers/router';
+import { Navbar } from '@/widgets/Navbar';
+import { SideBar } from '@/widgets/SideBar';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { MainLayout } from '@/shared/layouts/MainLayout';
+import { AppLoaderLayout } from '@/shared/layouts/AppLoaderLayout';
+import { PageLoader } from '@/widgets/PageLoader';
+import { useAppToolbar } from './lib/useAppToolbar';
 
 function App() {
   const { theme } = useTheme();
   const dispatch = useAppDispatch();
   const inited = useSelector(getUserInited);
+  const toolbar = useAppToolbar();
 
   useEffect(() => {
     if (!inited) {
@@ -25,44 +27,51 @@ function App() {
 
   if (!inited) {
     return (
-      <ToggleFeatures
-        feature="isAppRedesigned"
-        on={
-          <div id="app" className={classNames("app_redesigned", {}, [theme])}>
-            <AppLoaderLayout />
-          </div>
-        }
-        off={<PageLoader />}
-      />
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+              <div
+                  id="app"
+                  className={classNames('app_redesigned', {}, [theme])}
+              >
+                <AppLoaderLayout />
+              </div>
+            }
+            off={<PageLoader />}
+        />
     );
   }
 
   return (
-    <ToggleFeatures
-      feature="isAppRedesigned"
-      off={
-        <div id="app" className={classNames("app", {}, [theme])}>
-          <Suspense fallback="">
-            <Navbar />
-            <div className="content-page">
-              <SideBar />
-              <AppRouter />
+      <ToggleFeatures
+          feature="isAppRedesigned"
+          off={
+            <div id="app" className={classNames('app', {}, [theme])}>
+              <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                  <SideBar />
+                  <AppRouter />
+                </div>
+              </Suspense>
             </div>
-          </Suspense>
-        </div>
-      }
-      on={
-        <div id="app" className={classNames("app_redesigned", {}, [theme])}>
-          <Suspense fallback="">
-            <MainLayout
-              header={<Navbar />}
-              content={<AppRouter />}
-              sidebar={<SideBar />}
-            />
-          </Suspense>
-        </div>
-      }
-    />
+          }
+          on={
+            <div
+                id="app"
+                className={classNames('app_redesigned', {}, [theme])}
+            >
+              <Suspense fallback="">
+                <MainLayout
+                    header={<Navbar />}
+                    content={<AppRouter />}
+                    sidebar={<SideBar />}
+                    toolbar={toolbar}
+                />
+              </Suspense>
+            </div>
+          }
+      />
   );
 }
 
