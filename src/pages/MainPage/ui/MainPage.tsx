4632d2a33ff7a68525a7c1854c3615822dd4c03d
bender101 <sergkok227@gmail.com>
2024-01-16 10:44:41 +0300

    fixes

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Counter } from "@/entities/Counter";
 import { Page } from "@/widgets/Page";
-import { RatingCard } from "@/entities/Rating";
 
 const MainPage = () => {
   const { t } = useTranslation("main");
 
-  return (
-    <Page data-testid="MainPage">
-      {t("main")}
-      <div>
-        <Counter />
-        <RatingCard hasFeedback />
-      </div>
-    </Page>
-  );
+  return <Page data-testid="MainPage">{t("main")}</Page>;
 };
 
 export default MainPage;
