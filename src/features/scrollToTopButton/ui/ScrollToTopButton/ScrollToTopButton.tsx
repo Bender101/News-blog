543ac3f9da3f410b4b6c3c14bdd0e07e90020924
bender101 <sergkok227@gmail.com>
2024-01-17 12:29:21 +0300

    fixes

--- src/features/scrollToTopButton/ui/ScrollToTopButton/ScrollToTopButton.tsx
+++ src/features/scrollToTopButton/ui/ScrollToTopButton/ScrollToTopButton.tsx
@@ -1,28 +1,27 @@
-import { memo } from 'react';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import cls from './ScrollToTopButton.module.scss';
-import { Icon } from '@/shared/ui/redesigned/Icon';
-import CircleIcon from '@/shared/assets/icons/circle-up.svg';
+import { memo } from "react";
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { Icon } from "@/shared/ui/redesigned/Icon";
+import CircleIcon from "@/shared/assets/icons/circle-up.svg";
 
 interface ScrollToTopButtonProps {
-    className?: string;
+  className?: string;
 }
 
 export const ScrollToTopButton = memo((props: ScrollToTopButtonProps) => {
-    const { className } = props;
+  const { className } = props;
 
-    const onCLick = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
+  const onCLick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-    return (
-        <Icon
-            Svg={CircleIcon}
-            clickable
-            onClick={onCLick}
-            width={32}
-            height={32}
-            className={classNames(cls.ScrollToTopButton, {}, [className])}
-        />
-    );
+  return (
+    <Icon
+      Svg={CircleIcon}
+      clickable
+      onClick={onCLick}
+      width={32}
+      height={32}
+      className={classNames("", {}, [className])}
+    />
+  );
 });
--- src/shared/ui/deprecated/StarRating/StarRating.tsx
+++ src/shared/ui/deprecated/StarRating/StarRating.tsx
@@ -64,7 +64,7 @@ export const StarRating = memo((props: StarRatingProps) => {
                 [
                   currentStarsCount >= starNumber
                       ? cls.hovered
-                      : cls.normal,
+                      : cls.deprecated_normal,
                 ],
             ),
             Svg: StarIcon,
