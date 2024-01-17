f4d971fff98c48d4c2467c65b9f34a5c9d74a57a
bender101 <sergkok227@gmail.com>
2024-01-17 19:52:29 +0300

    fixes

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -5,7 +5,12 @@ import { Page } from "@/widgets/Page";
 const MainPage = () => {
   const { t } = useTranslation("main");
 
-  return <Page data-testid="MainPage">{t("main")}</Page>;
+  return <Page data-testid="MainPage">
+    <div>{t("main")}</div>
+    <div>Логин, пароль:</div>
+    <div>1. admin, 123</div>
+    <div>2. user,123</div>
+  </Page>;
 };
 
 export default MainPage;
